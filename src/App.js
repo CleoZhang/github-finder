21db51036be54d11bed7b0d490912561e2778935
Cleo Zhang <forest-zhang@hotmail.com>
2020-05-07 16:28:35 -0700

    Add Clear Button

--- src/App.js
+++ src/App.js
@@ -19,12 +19,20 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  clearUsers = e => {
+    this.setState({ users: [], loading: false });
+  };
+
   render() {
     return (
       <div className='App'>
         <NavBar />
         <div className='container'>
-          <Search searchUsers={this.searchUsers} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+          />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>

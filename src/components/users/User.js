ae72dc708c794291fb83ec143ef5e007b2dd1f12
Cleo Zhang <forest-zhang@hotmail.com>
2020-05-08 15:05:16 -0700

    Add User component

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
+import Spinner from '../layout/Spinner';
 
 export class User extends Component {
+  componentDidMount() {
+    this.props.getUser(this.props.match.params.login);
+  }
+
   render() {
-    return <div>User</div>;
+    if (this.props.user != null) {
+      const {
+        name,
+        avatat_url,
+        location,
+        bio,
+        blog,
+        login,
+        html_url,
+        followers,
+        following,
+        public_repos,
+        public_gists,
+        hireable,
+      } = this.props.user;
+
+      return <div>{name}</div>;
+    }
+
+    return <Spinner />;
   }
 }
 

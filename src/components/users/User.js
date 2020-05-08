38fa569b3b04be125b985f77aac615b5a68fe80c
Cleo Zhang <forest-zhang@hotmail.com>
2020-05-08 15:37:39 -0700

    Add User divs

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import propTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
 import Spinner from '../layout/Spinner';
 
 export class User extends Component {
@@ -10,7 +13,7 @@ export class User extends Component {
     if (this.props.user != null) {
       const {
         name,
-        avatat_url,
+        avatar_url,
         location,
         bio,
         blog,
@@ -18,16 +21,91 @@ export class User extends Component {
         html_url,
         followers,
         following,
+        company,
         public_repos,
         public_gists,
         hireable,
       } = this.props.user;
 
-      return <div>{name}</div>;
+      return (
+        <Fragment>
+          {name}
+          <Link to='/' className='btn btn-light'>
+            Back to Search
+          </Link>
+          Hireable:{' '}
+          {hireable ? (
+            <i className='fas fa-check text-success' />
+          ) : (
+            <i className='fas fa-times-circle text-danger' />
+          )}
+          <div className='card grid-2'>
+            <div className='all-center'>
+              <img
+                src={avatar_url}
+                className='round-img'
+                alt=''
+                style={{ width: '150px' }}
+              />
+              <h1>{name}</h1>
+              <p>Location: {location}</p>
+            </div>
+            <div>
+              {bio && (
+                <Fragment>
+                  <h3>Bio</h3>
+                  <p>{bio}</p>
+                </Fragment>
+              )}
+              <a href={html_url} className='btn btn-dark my-1'>
+                Visit Github Profile
+              </a>
+              <ul>
+                <li>
+                  {login && (
+                    <Fragment>
+                      <strong> Username: </strong>
+                      {login}
+                    </Fragment>
+                  )}
+                </li>
+                <li>
+                  {company && (
+                    <Fragment>
+                      <strong>Company: </strong>
+                      {company}
+                    </Fragment>
+                  )}
+                </li>
+                <li>
+                  {blog && (
+                    <Fragment>
+                      <strong>Website:</strong>
+                      {blog}
+                    </Fragment>
+                  )}
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className='card text-center'>
+            <div className='badge badge-primary'> Followers: {followers}</div>
+            <div className='badge badge-success'> Following: {following}</div>
+            <div className='badge badge-pending'>
+              Public Repos: {public_repos}
+            </div>
+            <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+          </div>
+        </Fragment>
+      );
     }
 
     return <Spinner />;
   }
 }
+
+User.propTypes = {
+  getUser: propTypes.func.isRequired,
+};
 
 export default User;
